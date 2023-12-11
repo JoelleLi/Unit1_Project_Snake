a3297e56c82d3276be61550e21774c240e3bf97f
joelleli <joelle.licho@rocketmail.com>
2023-12-11 21:35:50 +0000

    Everything established except start again option and making food spawn where snake is not

--- js/script.js
+++ js/script.js
@@ -1,209 +1,202 @@
-// Board configuration
-const width = 10
-const height = 10
-const cellCount = width * height
-let cells = []
-
-const startingPosition = 0
-let currentPosition = startingPosition
-let newPosition = 0
-let startingFoodPosition = 0
-let newFoodPosition = 0
-let currentScore = 0
-let currentDirection = "up"
-let foodPosition = 0
-let catBody = [1, 0]
-let collision = false
-let intervalDuration = 600
-let gameState 
-
 function init() {
-// Create grid
-const grid = document.querySelector(".grid")
-createGrid()
-function createGrid() {
-    //Use the cellcount to create our grid cells
-    for (let i = 0; i < cellCount; i++) {
-        const cell = document.createElement("div")
-        // Creates 100 divs first
 
-        // Assign index to div element 1-100
-        cell.innerText = i
-        // can also do cell.id = 1
+    // Board configuration
+    const width = 20
+    const height = 20
+    const cellCount = width * height
+    let cellsIndex = []
 
-        cell.id = i 
+    const startingPosition = 54
 
-        // Add the height and width to each grid cell using JS
-        cell.style.height = `${100 / height}`
-        cell.style.width = `${100 / width}`
+    let currentPosition = startingPosition
+    let newPosition = 0
+    let startingFoodPosition = 0
+    let currentScore = 0
+    let currentDirection = 1
+    let foodPosition = 0
+    let caterpillar = [54, 53, 52]
+    let collision = false
 
-        // Add cell to grid
-        grid.appendChild(cell)
+    // Create grid
+    const grid = document.querySelector(".grid")
+    function createGrid() {
+        //Use the cellcount to create our grid cells
+        for (let i = 0; i < cellCount; i++) {
+            const cell = document.createElement("div")
+            // Creates 100 divs first
 
-        // Add newly created div cell to cells array
-        cells.push(cell)
-    }
-    // Add cat class to starting position of cell
-    addFood()
-    render()
-    }
-}
+            // Assign index to div element 1-100
+            // cell.innerText = i
 
-setInterval(updateFoodPosition, 10)
+            // can also do cell.dataset.cellid = i
+            cell.id = i 
 
-setInterval(render, 600)
+            // Add the height and width to each grid cell using JS
+            cell.style.height = `${100 / height}`
+            cell.style.width = `${100 / width}`
 
-function render() {
-    updateFoodPosition()
-    updateCatPosition()
-    // checkIfCollision()
-    // renderMessage()
+            // Add cell to grid
+            grid.appendChild(cell)
 
-    const maxSpeed = 100
-    intervalDuration = Math.max(maxSpeed, 600 - currentScore * 10)
-    clearInterval(renderInterval)
-    renderInterval = setInterval(render, intervalDuration)
-}
-
-let renderInterval = setInterval(render, intervalDuration)
-
-function addFood() {
-    startingFoodPosition = (Math.floor((Math.random() * 99) + 1))
-    cells[startingFoodPosition].classList.add("food")
-    foodPosition = startingFoodPosition
-}
-
-function removeFood() {
-    cells[foodPosition].classList.remove("food")
-}
-
-function updateFoodPosition() {
-    if (cells[currentPosition].classList.contains("food")) {
-        removeFood()
+            // Add newly created div cell to cells array
+            cellsIndex.push(cell)
+        }
+        // Add cat class to starting position of cell
         addFood()
-        updatePoints()
-        growCatBody()
+        render()
     }
-}
+    createGrid()
 
-function removeCat() {
-    cells.forEach((cell) => cell.classList.remove("cat"))
-}
-
-function addCat() {
-    cells[currentPosition].classList.add("cat")
-}
-
-function growCatBody() {
-    catBody.push(catBody.length)
-    console.log("grow")
-}
-
-function updateCatDirection(event) {
-    const key = event.keyCode
-    const up = 38
-    const down = 40
-    const left = 37
-    const right = 39
-
-    // Remove caterpillar from previous position before updating current position to new cell
-    removeCat() // Remove caterpillar BEFORE current position is updated
-
-    if (key === up && currentDirection !== ("down" || "up")) {
-        currentPosition -= width
-        currentDirection = -width
-    } else if (key === down && currentDirection !== ("up" || "down")) {
-        currentPosition += width
-        currentDirection = width
-    } else if (key === left && currentDirection !== ("right" || "left")) {
-        currentPosition--
-        currentDirection = -1
-    } else if (key === right && currentDirection !== ("left" || "right")) {
-        currentPosition++
-        currentDirection = 1
-    } else {
-        console.log("INVALID KEY")
+    function addCat () {
+        document.querySelectorAll('.grid > div').forEach(cell => cell.classList.remove("cat")) 
+        //clears the grid before moving the caterpillar
+        for (let i = 0; i < caterpillar.length; i++){
+            cellsIndex[caterpillar[i]].classList.add("cat")
+            }
     }
-    // Add cat class once currentPosition has been updated
-    addCat(currentPosition)    
-}
+    // put caterpillar in starting position on board
+    addCat()
 
-function updateCatPosition() {
+    let startingIntervalDuration = 400
+    const reduceIntervalDuration = 0.95
+    let intervalDuration = startingIntervalDuration
+    let newIntervalDuration = 0
 
-    if (currentDirection === -width) {
-        removeCat()
-        newPosition = currentPosition - width
-    } else if (currentDirection === width) {
-        removeCat()
-        newPosition = currentPosition + width
-    } else if (currentDirection === -1) {
-        removeCat()
-        newPosition = currentPosition - 1
-    } else if (currentDirection === 1) {
-        removeCat()
-        newPosition = currentPosition + 1
-    } 
-    currentPosition = newPosition
-    catBody.unshift(currentPosition)
-    // console.log(catBody)
-    catBody.pop()
-    catBody.forEach(cell => (cells[cell].classList.add("cat"))) // makes caterpillar visible
+    let gameLoop = setInterval(render, intervalDuration)
 
-    console.log(catBody)
-}
+    function render(){
+        checkIfCollision()
+        updateCatPosition()
+        updateFoodPosition()      
+    }
 
-function checkIfCollision() {
-//     const catHead = catBody[0]
+    function changeGameLoop(){
+        clearInterval(gameLoop)
+        const increasedSpeed = intervalDuration * reduceIntervalDuration
+        newIntervalDuration = Math.max(increasedSpeed, 50)
+        console.log("speed = " + newIntervalDuration)
 
-//     if ((catBody[0] + width >= width * width && currentDirection === width) || // hits bottom wall
-//     (catBody[0] % width === width - 1 && currentDirection === 1) || // hits right wall
-//     (catBody[0] % width === 0 && currentDirection === -1) || // hits left wall
-//     (catBody[0] - width < 0 && currentDirection === -width)) {
-//         console.log("collision with wall")
-//         collision = true
-//         renderMessage()
-//     }
-    //*-----------------------------------------------------*
-    // const catHeadRow = Math.floor(catHead / width);
-    // const catHeadCol = catHead % width;
+        intervalDuration = newIntervalDuration
+        gameLoop = setInterval(render, intervalDuration)
+    }
 
-    // const isOutsideLeftBoundary = catHeadCol === 0 // and render happens again
-    // const isOutsideRightBoundary = catHeadCol === width - 1
-    // const isOutsideTopBoundary = catHeadRow === 0
-    // const isOutsideBottomBoundary = catHeadRow === Math.floor((cellCount - 1) / width)
+    document.addEventListener("keydown", updateCatDirection)
 
-    // if (isOutsideLeftBoundary || isOutsideRightBoundary || isOutsideTopBoundary || isOutsideBottomBoundary) {
-    //     collision = true
-    //     console.log("collision with wall")
-    // }
-    //*-----------------------------------------------------*
-    // Check if the cat's head collides with its own body
-    for (let i = 1; i < catBody.length; i++) {
-        if (catHead === catBody[i]) {
-            console.log("collision with body")
-            collision = true
+    function updateCatDirection(event) {
+        const key = event.keyCode
+        const up = 38
+        const down = 40
+        const left = 37
+        const right = 39
+        // Remove caterpillar from previous position before updating current position to new cell
+        // removeCat() // Remove caterpillar BEFORE current position is updated
+
+        if (key === up && currentDirection !== width) {
+            // currentPosition -= width
+            currentDirection = -width
+        } else if (key === down && currentDirection !== -width) {
+            // currentPosition += width
+            currentDirection = width
+        } else if (key === left && currentDirection !== +1) {
+            // currentPosition--
+            currentDirection = -1
+        } else if (key === right && currentDirection !== -1) {
+            // currentPosition++
+            currentDirection = 1
+        } else {
+            console.log("INVALID KEY")
+        }
+        // Add cat class once currentPosition has been updated
+        // addCat()    
+    }
+
+    function addFood() {
+        startingFoodPosition = (Math.floor((Math.random() * 99) + 1))
+        cellsIndex[startingFoodPosition].classList.add("food")
+        foodPosition = startingFoodPosition
+    }
+
+    function removeFood() {
+        cellsIndex[foodPosition].classList.remove("food")
+    }
+
+    // setInterval(updateFoodPosition, 10)
+    function updateFoodPosition() {
+        if (cellsIndex[currentPosition].classList.contains("food")) {
+            caterpillar.push(caterpillar.length)
+            console.log("grow")
+            removeFood()
+            addFood()
+            updatePoints()
         }
     }
-    return collision
+
+    function removeCat() {
+        cellsIndex.forEach((cell) => cell.classList.remove("cat"))
+    }
+
+    function updateCatPosition() {
+
+        if (currentDirection === -width) { // up
+            removeCat()
+            newPosition = currentPosition - width
+        } else if (currentDirection === width) { // down
+            removeCat()
+            newPosition = currentPosition + width
+        } else if (currentDirection === -1) { // left
+            removeCat()
+            newPosition = currentPosition - 1
+        } else if (currentDirection === +1) { // right
+            removeCat()
+            newPosition = currentPosition + 1
+        } 
+        currentPosition = newPosition
+        caterpillar.unshift(currentPosition)
+        // console.log(catBody)
+        caterpillar.pop()
+        caterpillar.forEach(cell => (cellsIndex[cell].classList.add("cat"))) // makes caterpillar visible
+        console.log(caterpillar)
+
+    }
+
+    function checkIfCollision() {
+        const catHead = caterpillar[0]
+        console.log("checking for collision")
+        // Check if caterpillar collides with walls
+        if (catHead + currentDirection < 0 || // caterpillar's head is above the top boundary
+            catHead + currentDirection >= cellCount || // caterpillar's head is below the bottom boundary
+            catHead % width === 0 && currentDirection === -1|| // caterpillar's head is on the left edge
+            (catHead + 1) % width === 0 && currentDirection === 1 || // caterpillar's head is on the right edge
+            cellsIndex[catHead + currentDirection].classList.contains("cat")) {
+                console.log("collision with walls")
+                collision = true
+                renderMessage()
+                console.log(catHead)
+            }
+        // Check if the cat's head collides with its own body
+        for (let i = 1; i < caterpillar.length; i++) {
+            if (catHead === caterpillar[i]) {
+                console.log("collision with body")
+                collision = true
+                renderMessage()
+            }
+        }
+    }
+
+    function updatePoints() {
+        currentScore++
+        changeGameLoop()
+        document.getElementById("score").innerText = `score : ${currentScore}`
+        console.log("points: " + currentScore)
+    }
+
+    function renderMessage() {
+        if (collision === true) {
+            console.log("GAME OVER")
+            clearInterval(gameLoop)
+            return
+        
+        }   
+    }
 }
-
-function updatePoints() {
-    currentScore++
-    document.getElementById("score").innerText = `score : ${currentScore}`
-    console.log("points: " + currentScore)
-}
-
-// function renderMessage() {
-//     const collision = checkIfCollision()
-//     if (collision === true) {
-//         if (confirm("GAME OVER, PLAY AGAIN?")) {
-//             console.log("you pressed ok")
-//             location.reload()
-//         } 
-//     }   return
-// }
-
-// ! EVENTS
-document.addEventListener("keydown", updateCatDirection)
-// ! PAGE LOAD
 window.addEventListener("DOMContentLoaded", init)
